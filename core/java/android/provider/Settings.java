a695b3c41cc84c9a1c1b00f71c1fb13b0530088a
htchoi1995 <htchoi1995@gmail.com>
2013-03-14 11:52:43 +0900

    Fix build error

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2402,14 +2402,6 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_ICON = "statusbar_battery_icon";
 
         /**
-         * Control the display of the action overflow button within app UI.
-         * 0 = use system default
-         * 1 = force on
-         * @hide
-         */
-        public static final String UI_FORCE_OVERFLOW_BUTTON = "ui_force_overflow_button";
-
-        /**
          * AM/PM Style for clock options
          * 0 - Normal AM/PM
          * 1 - Small AM/PM
